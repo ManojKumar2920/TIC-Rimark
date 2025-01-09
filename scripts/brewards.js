4b97a03c777a71b682a5cf1d7b560008d3242f79
shiva <shivatadigadapa@gmail.com>
2025-01-09 12:43:16 +0530

    Refactor UI components for improved styling and layout consistency

--- scripts/brewards.js
+++ scripts/brewards.js
@@ -149,8 +149,8 @@ const renderExpiredRewards = (data) => {
             (reward) => `
    <div class="max-w-md  mx-auto p-8 bg-white rounded-lg shadow-lg">
   <!-- Title -->
-  <h2 class="md:text-2xl whitespace-nowrap font-semibold text-gray-800 text-center">
-    Create reward <span class="text-blue-600">${reward.name}</span>
+  <h2 class="md:text-2xl text-xl whitespace-nowrap font-semibold  text-gray-800 text-center">
+    Create reward <span class="text-[#1F41BB]">${reward.name}</span>
   </h2>
   <!-- Subtitle -->
   <p class="text-gray-500 text-sm  md:text-base text-xs  text-center mt-2">
